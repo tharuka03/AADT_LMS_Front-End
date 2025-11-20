801944398664ff25191dd2b5fbb41a917b4c959a
Kalindu Tharanga <71347341+kalinduTh@users.noreply.github.com>
2025-11-20 23:14:59 +0530

    fix: primary Style import.

--- src/Pages/Course/CoursePageCertificate.jsx
+++ src/Pages/Course/CoursePageCertificate.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
-import { primaryStyles } from '../../Styles/';
+import { primaryStyles } from '../../Styles/primaryStyles';
 
 function CoursePageCertificate() {
     const { courseID } = useParams();
